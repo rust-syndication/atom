7f90336fa692393d906ac498818cad7742e8f70f
Steven Sheldon <steven@sasheldon.com>
2020-01-06 08:46:06 -0800

    Handwrite Error conformance so thiserror is not needed (#18)
    * Remove dependency on thiserror
    
    * Conform to rustfmt
    
    * Use newer StdError API and ensure no ambiguity in fmt

--- src/error.rs
+++ src/error.rs
@@ -1,24 +1,60 @@
+use std::error::Error as StdError;
+use std::fmt;
 use std::str::Utf8Error;
 
 use quick_xml::Error as XmlError;
-use thiserror::Error;
 
-#[derive(Debug, Error)]
+#[derive(Debug)]
 /// An error that occurred while performing an Atom operation.
 pub enum Error {
     /// Unable to parse XML.
-    #[error("{0}")]
-    Xml(#[from] XmlError),
+    Xml(XmlError),
     /// Unable to parse UTF8 in to a string.
-    #[error("{0}")]
-    Utf8(#[from] Utf8Error),
+    Utf8(Utf8Error),
     /// Input did not begin with an opening feed tag.
-    #[error("input did not begin with an opening feed tag")]
     InvalidStartTag,
     /// Unexpected end of input.
-    #[error("unexpected end of input")]
     Eof,
     /// The format of the timestamp is wrong.
-    #[error("timestamps must be formatted by RFC3339, rather than {0}")]
     WrongDatetime(String),
+}
+
+impl StdError for Error {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        match *self {
+            Error::Xml(ref err) => Some(err),
+            Error::Utf8(ref err) => Some(err),
+            Error::InvalidStartTag => None,
+            Error::Eof => None,
+            Error::WrongDatetime(_) => None,
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::Xml(ref err) => fmt::Display::fmt(err, f),
+            Error::Utf8(ref err) => fmt::Display::fmt(err, f),
+            Error::InvalidStartTag => write!(f, "input did not begin with an opening feed tag"),
+            Error::Eof => write!(f, "unexpected end of input"),
+            Error::WrongDatetime(ref datetime) => write!(
+                f,
+                "timestamps must be formatted by RFC3339, rather than {}",
+                datetime
+            ),
+        }
+    }
+}
+
+impl From<XmlError> for Error {
+    fn from(err: XmlError) -> Error {
+        Error::Xml(err)
+    }
+}
+
+impl From<Utf8Error> for Error {
+    fn from(err: Utf8Error) -> Error {
+        Error::Utf8(err)
+    }
 }
