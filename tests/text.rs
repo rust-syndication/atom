628623198d3b464e46cd2ce137e7c2d3f467125c
Andrey Kutejko <andy128k@gmail.com>
2021-03-16 20:32:15 +0100

    update tests

--- tests/text.rs
+++ tests/text.rs
@@ -1,4 +1,4 @@
-use atom_syndication::{Feed, Text};
+use atom_syndication::{Feed, Text, TextType};
 use std::fs::File;
 use std::io::BufReader;
 
@@ -13,26 +13,42 @@ macro_rules! feed {
 #[test]
 fn text_cdata_escaped() {
     let feed = feed!("tests/data/text_cdata_escaped.xml");
-    assert_eq!(feed.title(), "&lt;p&gt;Feed Title&lt;/p&gt;");
+    let title = feed.title();
+    assert_eq!(title, "&lt;p&gt;Feed Title&lt;/p&gt;");
+    assert_eq!(title.base, None);
+    assert_eq!(title.lang, None);
+    assert_eq!(title.r#type, TextType::Html);
 }
 
 #[test]
 fn text_cdata() {
     let reader = BufReader::new(File::open("tests/data/text_cdata.xml").unwrap());
     let feed = Feed::read_from(reader).unwrap();
-    assert_eq!(feed.title(), "<p>Feed Title</p>");
+    let title = feed.title();
+    assert_eq!(title, "<p>Feed Title</p>");
+    assert_eq!(title.base, None);
+    assert_eq!(title.lang, None);
+    assert_eq!(title.r#type, TextType::Html);
 }
 
 #[test]
 fn text_empty() {
     let feed = feed!("tests/data/text_empty.xml");
-    assert_eq!(feed.title(), "");
+    let title = feed.title();
+    assert_eq!(title, "");
+    assert_eq!(title.base, None);
+    assert_eq!(title.lang, None);
+    assert_eq!(title.r#type, TextType::Text);
 }
 
 #[test]
 fn text_html() {
     let feed = feed!("tests/data/text_html.xml");
-    assert_eq!(feed.title(), "<p>Feed Title</p>");
+    let title = feed.title();
+    assert_eq!(title, "<p>Feed Title</p>");
+    assert_eq!(title.base, None);
+    assert_eq!(title.lang, None);
+    assert_eq!(title.r#type, TextType::Html);
 }
 
 #[test]
@@ -41,6 +57,27 @@ fn text_write_html() {
     feed.set_title(Text::html("<p>Feed Title</p>"));
     let xml = feed.to_string();
     assert!(xml.contains(r#"<title type="html">&lt;p&gt;Feed Title&lt;/p&gt;</title>"#));
+}
+
+#[test]
+fn text_write_html_with_lang() {
+    let mut feed = Feed::default();
+    let mut title = Text::html("<p>Feed Title</p>");
+    title.lang = Some("en".to_string());
+    feed.set_title(title);
+    let xml = feed.to_string();
+    assert!(xml.contains(r#"<title lang="en" type="html">&lt;p&gt;Feed Title&lt;/p&gt;</title>"#));
+}
+
+#[test]
+fn text_write_html_with_base_and_lang() {
+    let mut feed = Feed::default();
+    let mut title = Text::html("<p>Feed Title</p>");
+    title.base = Some("http://example.com/articles/".to_string());
+    title.lang = Some("en".to_string());
+    feed.set_title(title);
+    let xml = feed.to_string();
+    assert!(xml.contains(r#"<title base="http://example.com/articles/" lang="en" type="html">&lt;p&gt;Feed Title&lt;/p&gt;</title>"#));
 }
 
 #[test]
@@ -54,29 +91,45 @@ fn text_write_xhtml() {
 #[test]
 fn text_plain() {
     let feed = feed!("tests/data/text_plain.xml");
-    assert_eq!(feed.title(), "Feed Title");
+    let title = feed.title();
+    assert_eq!(title, "Feed Title");
+    assert_eq!(title.base, None);
+    assert_eq!(title.lang, None);
+    assert_eq!(title.r#type, TextType::Text);
 }
 
 #[test]
 fn text_plain_escaped() {
     let feed = feed!("tests/data/text_plain_escaped.xml");
-    assert_eq!(feed.title(), "<p>Feed Title</p>");
+    let title = feed.title();
+    assert_eq!(title, "<p>Feed Title</p>");
+    assert_eq!(title.base, None);
+    assert_eq!(title.lang, None);
+    assert_eq!(title.r#type, TextType::Text);
 }
 
 #[test]
 fn text_xhtml_escaped() {
     let feed = feed!("tests/data/text_xhtml_escaped.xml");
+    let title = feed.title();
     assert_eq!(
-        feed.title(),
+        title,
         "<div xmlns=\"http://www.w3.org/1999/xhtml\">&lt;p&gt;Feed Title&lt;/p&gt;</div>"
     );
+    assert_eq!(title.base, None);
+    assert_eq!(title.lang, None);
+    assert_eq!(title.r#type, TextType::Xhtml);
 }
 
 #[test]
 fn text_xhtml() {
     let feed = feed!("tests/data/text_xhtml.xml");
+    let title = feed.title();
     assert_eq!(
-        feed.title(),
+        title,
         "<div xmlns=\"http://www.w3.org/1999/xhtml\"><p>Feed Title</p></div>"
     );
+    assert_eq!(title.base, None);
+    assert_eq!(title.lang, None);
+    assert_eq!(title.r#type, TextType::Xhtml);
 }
